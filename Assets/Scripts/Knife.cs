e8cdd6f49b960aa68f0248ac814f5d03c6522e8f
DeimisM <deimis.mardosas@outlook.com>
2024-02-01 19:19:15 +0200

    knife

---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPos.z = 0;
+        
+        //transform.position = worldPos;
+        rb.MovePosition(worldPos);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
