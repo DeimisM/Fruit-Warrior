e832cba6fc6da6dcdf897ac4eaf8361dc0693c18
DeimisM <deimis.mardosas@outlook.com>
2024-02-08 19:41:51 +0200

    lists in lists

---
+++ Assets/Scripts/FruitSpawnData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FruitSpawnData
+{
+    public float delay;
+    public bool isBomb;
+    public float x;
+    public Vector2 velocity;
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public List<GameObject> fruits;
+    //public List<GameObject> fruits;
+    public GameObject bombPrefab;
+    public GameObject fruitPrefab;
+    public List<Wave> waves;
 
-    void Start()
+
+    async void Start()
     {
-        InvokeRepeating(nameof(Spawn), 1f, 1f);
+        foreach (var wave in waves)
+        {
+            foreach(var fruit in wave.items)
+            {
+                var prefab = fruit.isBomb ? bombPrefab : fruitPrefab;    // sutrumpintas if'as
+                var go = Instantiate(prefab);
+                go.transform.position = new Vector3(fruit.x, -5f, 0);
+
+                await new WaitForSeconds(3f);
+            }
+        }
+        //InvokeRepeating(nameof(Spawn), 1f, 1f);
     }
 
+    /*
     void Spawn()
     {
         var fruit = fruits[Random.Range(0, fruits.Count)];
@@ -19,4 +35,5 @@ public class Spawner : MonoBehaviour
 
         obj.transform.position = new Vector3(Random.Range(-6f, 6f), Random.Range(-6f, -3f), 0);
     }
+    */
 }
